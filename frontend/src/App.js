9ee27b990e659c1650b441df87a881904f2223eb
祝井直樹 <nok181701@gmail.com>
2024-01-05 23:36:41 +0900

    🔥 既存のCSS import削除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import Home from "src/pages/Home";
-import "./App.css";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "src/pages/Home";
+import "src/styles/tailwind.css";
 
 function App() {
   return (
