746dae0b09a9040d1e31d0f56569974b590d2c04
祝井直樹 <nok181701@gmail.com>
2024-01-09 16:56:26 +0900

    ✨ 現在地に移動にする機能を実装

--- frontend/src/components/Main/MapComponent/MapComponent.jsx
+++ frontend/src/components/Main/MapComponent/MapComponent.jsx
@@ -1,5 +1,7 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { HashLoader } from "react-spinners";
+import "src/styles/Main/MapComponent/MapComponent.css";
 
 const MapComponent = () => {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -9,29 +11,20 @@ const MapComponent = () => {
     height: "400px",
     borderRadius: "15px",
     overflow: "hidden",
+    position: "relative",
   };
 
   const [currentPosition, setCurrentPosition] = useState(null);
-  const storedPosition = JSON.parse(localStorage.getItem("currentPosition"));
+  const [isLoading, setIsLoading] = useState(false);
 
-  const defaultPosition = useMemo(
-    () => storedPosition || { lat: 35.6895, lng: 139.6917 },
-    [storedPosition]
-  );
+  const defaultPosition = useMemo(() => ({ lat: 35.6895, lng: 139.6917 }), []);
 
   const getCurrentLocation = useCallback(async () => {
     try {
+      setIsLoading(true);
       const position = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
       });
-
-      localStorage.setItem(
-        "currentPosition",
-        JSON.stringify({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        })
-      );
 
       setCurrentPosition({
         lat: position.coords.latitude,
@@ -40,16 +33,22 @@ const MapComponent = () => {
     } catch (e) {
       console.log(`取得できませんでした`);
       setCurrentPosition(defaultPosition);
+    } finally {
+      setIsLoading(false);
     }
   }, [defaultPosition]);
 
-  useEffect(() => {
-    getCurrentLocation();
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <LoadScript googleMapsApiKey={apiKey}>
       <div style={containerStyle}>
+        {isLoading && (
+          <div className="loadingOverlay">
+            <HashLoader color="#60A5FA" width={8} />
+            <p className="p-2 ml-2 font-bold">Loading...</p>
+          </div>
+        )}
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={currentPosition || defaultPosition}
@@ -57,6 +56,12 @@ const MapComponent = () => {
         >
           {currentPosition && <Marker position={currentPosition} />}
         </GoogleMap>
+        <button
+          onClick={getCurrentLocation}
+          className="getCurrentLocationButton"
+        >
+          現在地に移動する
+        </button>
       </div>
     </LoadScript>
   );
