0870905e46eb83a3fb28eccfff9a90860395a58e
祝井直樹 <nok181701@gmail.com>
2024-01-12 17:00:30 +0900

    ✨ サークル内の店舗を計算

--- frontend/src/components/Main/MapComponent/index.jsx
+++ frontend/src/components/Main/MapComponent/index.jsx
@@ -1,8 +1,10 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import { useCallback, useMemo, useRef, useState } from "react";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { HashLoader } from "react-spinners";
 import CircleAndMaker from "src/components/Main/MapComponent/CircleAndMaker";
 import "src/styles/Main/MapComponent/MapComponent.css";
+import dummyData from "src/dummyData.json";
+import haversineDistance from "haversine-distance";
 
 const MapComponent = () => {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
@@ -17,14 +19,21 @@ const MapComponent = () => {
 
   const [currentPosition, setCurrentPosition] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [mapCenter, setMapCenter] = useState(null);
+  const [markers, setMarkers] = useState([]);
   const defaultPosition = useMemo(() => ({ lat: 35.6812, lng: 139.7671 }), []);
   const mapRef = useRef(null);
 
   const handleLoad = (map) => {
     mapRef.current = map;
+    if (!mapCenter && map) {
+      //ローディング時にサークル中心の緯度経度を取得
+      setMapCenter(map.getCenter().toJSON());
+    }
   };
 
   const handleCenterChanged = useCallback(() => {
+    //マップ上の中心座標が変更したときにピンとサークルを再び中心座標に移動させる
     if (!mapRef.current) return;
 
     if (handleCenterChanged.timer) {
@@ -46,6 +55,7 @@ const MapComponent = () => {
   }, [setCurrentPosition, currentPosition]);
 
   const getCurrentLocation = useCallback(async () => {
+    //現在位置の取得
     try {
       setIsLoading(true);
       const position = await new Promise((resolve, reject) => {
@@ -63,6 +73,47 @@ const MapComponent = () => {
       setIsLoading(false);
     }
   }, [defaultPosition]);
+
+  const searchNearbyPlaces = useCallback(async (location) => {
+    //経度緯度の２点間の距離を求める
+    const centerCoordinates = {
+      latitude: location.lat,
+      longitude: location.lng,
+    };
+    const radiusInMeters = 1000;
+
+    const placesInRadius = dummyData.filter((place) => {
+      const placeCoordinates = { latitude: place.lat, longitude: place.lng };
+      const distance = haversineDistance(centerCoordinates, placeCoordinates);
+
+      return distance <= radiusInMeters;
+    });
+    return placesInRadius;
+  }, []);
+
+  useEffect(() => {
+    // 初回描画時や currentPosition が変更されたときに検索を行う
+    const performSearch = async () => {
+      try {
+        setIsLoading(true);
+
+        const places = await searchNearbyPlaces(
+          currentPosition || defaultPosition
+        );
+        const newMarkers = places.map((place) => ({
+          position: { lat: place.lat, lng: place.lng },
+          name: place.name,
+        }));
+        setMarkers(newMarkers);
+      } catch (error) {
+        console.error("Error searching nearby places:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    performSearch();
+  }, [currentPosition, defaultPosition, searchNearbyPlaces]);
 
   return (
     <LoadScript googleMapsApiKey={apiKey}>
@@ -85,6 +136,13 @@ const MapComponent = () => {
           ) : (
             <CircleAndMaker defaultPosition={defaultPosition} />
           )}
+          {markers.map((marker, index) => (
+            <MarkerF
+              key={index}
+              position={marker.position}
+              title={marker.name}
+            />
+          ))}
         </GoogleMap>
         <button
           onClick={getCurrentLocation}
