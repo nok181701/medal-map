d74471fe495cf0c7f1af39b78ea2a8e00e0e2e1b
祝井直樹 <nok181701@gmail.com>
2024-01-16 23:24:43 +0900

    ⚡️スマホ版でGoogleマップの高さを変更

--- frontend/src/components/Main/MapComponent/index.jsx
+++ frontend/src/components/Main/MapComponent/index.jsx
@@ -39,7 +39,7 @@ const MapComponent = (props) => {
       <div
         style={{
           width: "100%",
-          height: "400px",
+          height: window.innerWidth < 600 ? "330px" : "400px", //
           borderRadius: "15px",
           overflow: "hidden",
           position: "relative",
