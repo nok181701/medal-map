ccfda55c690d6237d5b6c0e315e08ffdca15d5ff
祝井直樹 <nok181701@gmail.com>
2024-01-18 13:57:18 +0900

    ⚡️ cors・josn化・パスの修正

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ const path = require("path"); // path モジュールをインポート
 const shopsRouter = require(path.join(__dirname, "routes", "shops")); // ファイルパスを安全に結合
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
-app.use("/shops", shopsRouter); // /places エンドポイントに placesRouter を使用
+app.use(cors());
+app.use(express.json());
+app.use("/api/shops", shopsRouter); // /places エンドポイントに placesRouter を使用
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
