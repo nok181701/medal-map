e04016a5871adff4deee58bc754b867a9f86ce11
祝井直樹 <nok181701@gmail.com>
2024-01-20 02:10:55 +0900

    パスの変更

--- frontend/src/components/Main/ShopsComponent/index.jsx
+++ frontend/src/components/Main/ShopsComponent/index.jsx
@@ -27,8 +27,9 @@ const ShopsComponent = (props) => {
               <div className="mb-4 md:mb-0 md:mr-4">
                 <img
                   src={
-                    marker.image
-                    // : `${imgUrl}/no-pictures.png` || "/no-pictures.png"
+                    marker.image !== null
+                      ? `${imgUrl}/${marker.image}`
+                      : `${imgUrl}/no-pictures.png` || "/no-pictures.png"
                   }
                   className="h-44 object-contain m-auto"
                   alt=""
