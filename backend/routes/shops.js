9f2b4246d2975fc5b61dea5cc4f85f3a681e77d9
祝井直樹 <nok181701@gmail.com>
2024-01-19 17:04:44 +0900

    開発環境設定にもどす

--- backend/routes/shops.js
+++ backend/routes/shops.js
@@ -6,11 +6,14 @@ const cors = require("cors");
 
 router.use(cors());
 
+// const pool = mysql.createPool(process.env.JAWSDB_URL);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
