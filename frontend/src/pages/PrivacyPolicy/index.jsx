989553f22a9b311f2afc7fa0cdd0b43e8729fc67
祝井直樹 <nok181701@gmail.com>
2024-01-22 00:12:15 +0900

    🩹 不要なので削除

--- frontend/src/pages/PrivacyPolicy/index.jsx
+++ frontend/src/pages/PrivacyPolicy/index.jsx
@@ -1,5 +1,3 @@
-// src/pages/PrivacyPolicy.js
-import React from "react";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 
--- frontend/src/pages/TermsOfService/index.jsx
+++ frontend/src/pages/TermsOfService/index.jsx
@@ -1,4 +1,3 @@
-// src/pages/TermsOfService.js
 import React from "react";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
