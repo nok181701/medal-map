40a0647656d4951fba4e5fc5921dea4449f9fbd4
祝井直樹 <nok181701@gmail.com>
2024-01-24 15:50:06 +0900

    ⚡️ アイコン押下時にもトップページ遷移

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import HumbergerMenu from "src/components/Header/ HumbergerMenu";
+import { Link } from "react-router-dom";
+import HamburgerMenu from "src/components/Header/HamburgerMenu";
 import "src/styles/Header/Header.css";
 
 const Header = () => {
@@ -8,20 +9,26 @@ const Header = () => {
   return (
     <header className="header-gradient p-4 relative z-10">
       <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <img
-            src={
-              process.env.REACT_APP_PUBLIC_URL === "development"
-                ? `${imgUrl}/logo.png`
-                : "/logo.png"
-            }
-            alt="Icon"
-            className=""
-            style={{ width: "42px", height: "42px", marginRight: "10px" }}
-          />
-          <h1 className="text-xl font-bold text-white">メダルマップ</h1>
+        <div className="flex items-center cursor-pointer">
+          <Link
+            to="/"
+            className="flex items-center"
+            style={{ textDecoration: "none" }}
+          >
+            <img
+              src={
+                process.env.REACT_APP_PUBLIC_URL === "development"
+                  ? `${imgUrl}/logo.png`
+                  : "/logo.png"
+              }
+              alt="Icon"
+              className=""
+              style={{ width: "42px", height: "42px", marginRight: "10px" }}
+            />
+            <h1 className="text-xl font-bold text-white">メダルマップ</h1>
+          </Link>
         </div>
-        <HumbergerMenu />
+        <HamburgerMenu />
       </div>
     </header>
   );
