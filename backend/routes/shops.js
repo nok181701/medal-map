ffdbc939412d02250f928e2e72bd65a6a11f1830
祝井直樹 <nok181701@gmail.com>
2024-01-26 14:30:16 +0900

    ⚡️ クエリ改善

--- backend/routes/shops.js
+++ backend/routes/shops.js
@@ -48,11 +48,11 @@ router.post(
     try {
       const { centerCoordinates, radiusInMeters } = req.body;
 
-      // 2地点間(サークルの中心座標とその範囲内の店舗)の計算
       const placesInRadius = await searchNearbyPlaces(
         centerCoordinates,
         radiusInMeters
       );
+
       const simplifiedPlaces = placesInRadius.map((place) => {
         return {
           type: place.type,
@@ -76,26 +76,29 @@ router.post(
 const searchNearbyPlaces = async (centerCoordinates, radiusInMeters) => {
   try {
     const query = `
-      SELECT
-        shops.id,
-        shops.type,
-        shops.name,
-        shops.address,
-        shops.phone_number,
-        shops.opening_hours,
-        shops.latitude,
-        shops.longitude,
-        shops.place_id,
-        shops_images.image,
-        haversineDistance(?, ?, shops.latitude, shops.longitude) AS distance
-      FROM
-        shops
-      LEFT JOIN
-        shops_images ON shops.id = shops_images.shop_id
-      HAVING
-        distance <= ?
-    `;
+    SELECT
+      shops.id,
+      shops.type,
+      shops.name,
+      shops.address,
+      shops.phone_number,
+      shops.opening_hours,
+      shops.latitude,
+      shops.longitude,
+      shops.place_id,
+      shops_images.image,
+      haversineDistance(?, ?, shops.latitude, shops.longitude) AS distance
+    FROM
+      shops
+    LEFT JOIN
+      shops_images ON shops.id = shops_images.shop_id
+    WHERE
+      haversineDistance(?, ?, shops.latitude, shops.longitude) <= ?
+  `;
+
     const [rows, fields] = await pool.execute(query, [
+      centerCoordinates.latitude,
+      centerCoordinates.longitude,
       centerCoordinates.latitude,
       centerCoordinates.longitude,
       radiusInMeters,
