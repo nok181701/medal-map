a79b4bd90c0ea1a9701db0512a23300def22043f
祝井直樹 <nok181701@gmail.com>
2024-02-12 17:05:56 +0900

    💄 アイコンやファビコンの種類

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const imgUrl = process.env.REACT_APP_PUBLIC_URL;
 
   return (
-    <header className="header-gradient p-4 relative z-10">
+    <header className="header-gradient p-2 relative z-10">
       <div className="flex justify-between items-center">
         <div className="flex items-center cursor-pointer">
           <Link
@@ -24,7 +24,7 @@ const Header = () => {
               }
               alt="Icon"
               className=""
-              style={{ width: "42px", height: "42px", marginRight: "10px" }}
+              style={{ width: "50px", height: "50px", marginRight: "10px" }}
             />
             <h1 className="text-xl font-bold text-white">メダルマップ</h1>
           </Link>
