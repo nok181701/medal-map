0e5140c27ee28db929fac12869a75446d814910c
祝井直樹 <nok181701@gmail.com>
2024-02-13 17:17:46 +0900

    ✨ Xへのリンク設置

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "src/styles/Footer/Footer.css";
 
 const Footer = () => {
+  const imgUrl = process.env.REACT_APP_PUBLIC_URL;
   return (
     <footer className="header-gradient p-8 text-white text-center">
       <div className="mb-4">
@@ -10,12 +11,27 @@ const Footer = () => {
           &copy; 2024 メダルマップ. All Rights Reserved.
         </p>
       </div>
-      <div>
+      <div className="flex items-center justify-center">
         <Link to="/terms" className="text-xs inline mr-4">
           利用規約
         </Link>
-        <Link to="/privacy" className="text-xs inline">
+        <Link to="/privacy" className="text-xs inline mr-4">
           プライバシーポリシー
+        </Link>
+        <Link
+          to="https://twitter.com/medalmap_info"
+          target="_blank"
+          className="text-xs inline"
+        >
+          <img
+            src={
+              process.env.REACT_APP_PUBLIC_URL === "development"
+                ? `${imgUrl}/logo-black.png`
+                : "/logo-black.png"
+            }
+            alt=""
+            className="w-5 h-5"
+          />
         </Link>
       </div>
     </footer>
