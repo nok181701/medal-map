ce32f98c7de7829e8fc0e5e126d4eb04c80db40e
祝井直樹 <nok181701@gmail.com>
2024-09-05 16:59:07 +0900

    fix home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,13 +1,44 @@
+import React, { useEffect, useState } from "react";
 import Footer from "src/components/Footer";
 import Header from "src/components/Header";
 import Main from "src/components/Main";
+import MaintenancePage from "src/components/Maintenance";
 
 const Home = () => {
+  const [isMaintenance, setIsMaintenance] = useState(false);
+
+  useEffect(() => {
+    // バックエンドの状態をチェックするAPIリクエスト
+    const checkBackendStatus = async () => {
+      try {
+        const response = await fetch("/api/status"); // バックエンドの状態を確認するエンドポイント
+        if (response.ok) {
+          const data = await response.json();
+          setIsMaintenance(data.maintenanceMode); // レスポンスの状態に応じて更新
+        } else {
+          // APIリクエストが失敗した場合
+          setIsMaintenance(true);
+        }
+      } catch (error) {
+        // エラーが発生した場合
+        setIsMaintenance(true);
+      }
+    };
+
+    checkBackendStatus();
+  }, []);
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      {isMaintenance ? (
+        <MaintenancePage />
+      ) : (
+        <>
+          <Header />
+          <Main />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
