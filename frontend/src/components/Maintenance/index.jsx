d31e7a3cade5c7d0cd1364cad8b4942a8da703aa
祝井直樹 <nok181701@gmail.com>
2024-09-05 16:58:52 +0900

    add page

---
+++ frontend/src/components/Maintenance/index.jsx
@@ -0,0 +1,10 @@
+const MaintenancePage = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>サービス停止中</h1>
+      <p>ただいまメンテナンス中です。しばらくお待ちください。</p>
+    </div>
+  );
+};
+
+export default MaintenancePage;
